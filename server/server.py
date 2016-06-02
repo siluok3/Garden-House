f3c795f7d820157a469386380d553adcfe5be390
RudolfGeosits <rgeosits@live.esu.edu>
2016-06-02 14:52:47 -0400

    Got it

--- server/server.py
+++ server/server.py
@@ -10,7 +10,8 @@ from threading import Thread
 temp = "NULL"
 humidity = "NULL"
 water = "NULL"
-bindata = 0
+bindata = None
+bin_done = False
 
 class MyServerProtocol(WebSocketServerProtocol):
 
@@ -23,16 +24,22 @@ class MyServerProtocol(WebSocketServerProtocol):
         #self.sendMessage("", False)
                 
     def onMessage(self, payload, isBinary):
-        global temp, humidity, water;
+        global temp, humidity, water, bindata, bin_done;
 
         if isBinary:
             print("Binary message received: {} bytes\n".format(len(payload)))
         else:
             print("Text message received: {}\n".format(payload.decode('utf8')))
             # echo back message verbatim
-            self.sendMessage(temp, False)
-            self.sendMessage(humidity, False)
-            self.sendMessage(water, False)
+            try:
+                if bin_done:
+                    bin_done = False
+                    self.sendMessage(bindata, True)
+                    self.sendMessage(temp, False)
+                    self.sendMessage(humidity, False)
+                    self.sendMessage(water, False)
+            except AssertionError:
+                pass
 
     def onClose(self, wasClean, code, reason):
         #print("WebSocket connection closed: {}\n".format(reason))
@@ -46,7 +53,7 @@ class MyServerProtocol(WebSocketServerProtocol):
 
 
 def func(i):
-    global temp, humidity, water, bindata;
+    global temp, humidity, water, bindata, bin_done;
 
     # Create a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -69,20 +76,53 @@ def func(i):
             print >>sys.stderr, 'connection from', client_address
         
             while True:
-                print "AWAITING DATA"
-                #bindata  = connection.recv(70000)
-                #print "got bindata"
-                temp     = connection.recv(32)
+                data_size = connection.recv(32)
+                uni_data_size = unicode(data_size, errors='ignore')
+
+                if (data_size == ''):
+                    continue
+
+                print 'data size = ' + data_size
+
+                bindata = connection.recv( int(data_size) )
+                print 'Got bindata size: ' + str(len(bindata))
+                total = len(bindata)
+
+                while True:
+                    if int(total) == int(data_size):
+                        print 'breaking w total of : ' + str(total)
+                        bin_done = True
+                        break
+
+                    new_data = connection.recv( int(str(data_size)) )
+                    bindata += new_data
+                    print 'Got bindata size: ' + str(len(new_data))
+                    total += len(new_data)
+                
+                time.sleep(5)
+
+                '''
+                bindata += connection.recv(10000)
+                bindata += connection.recv(10000)
+                bindata += connection.recv(10000)
+                bindata += connection.recv(10000)
+                bindata += connection.recv(10000)
+                bindata += connection.recv(10000)
+                '''
+
+                #bindata += connection.recv(60000)
+                #print "got bindata 2"
+                #temp     = connection.recv(32)
                 #print "got temp"
-                humidity = connection.recv(32)
+                #humidity = connection.recv(32)
                 #print "got hum"
-                water    = connection.recv(32)
+                #water    = connection.recv(32)
                 #print "got water"
                 #time.sleep(5)
                 #print >>sys.stderr, 'received "%s"' % bindata
-                print >>sys.stderr, 'received "%s"' % temp
-                print >>sys.stderr, 'received "%s"' % humidity
-                print >>sys.stderr, 'received "%s"' % water
+                #print >>sys.stderr, 'received "%s"' % temp
+                #print >>sys.stderr, 'received "%s"' % humidity
+                #print >>sys.stderr, 'received "%s"' % water
                 
             #if data:
             #    print >>sys.stderr, 'sending data back to the client'
@@ -90,7 +130,11 @@ def func(i):
             #else:
             #    print >>sys.stderr, 'no more data from', client_address
             #    break
-            
+        #except ValueError:
+            #pass
+        #except UnicodeDecodeError:
+            #pass
+                
         finally:
             print "finally"
             #Clean up the connection
--- webpage/garden.js
+++ webpage/garden.js
@@ -1,13 +1,90 @@
-// Socket Declarations
-var ws;
+var s = null;
+var on_pic = true, on_temp = false, on_hum = false, on_water = false;
 
-// Websock Declarations
-var ws = new WebSocket('ws://localhost:9000', 'garden-protocol');
+function encode (input) {
+    var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+    var output = "";
+    var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+    var i = 0;
 
-ws.onmessage = function (event) {
-    var msg = event.data;
+    while (i < input.length) {
+        chr1 = input[i++];
+        chr2 = i < input.length ? input[i++] : Number.NaN; // Not sure if the index
+        chr3 = i < input.length ? input[i++] : Number.NaN; // checks are needed here
 
-    switch (msg) {	
-	default: console.log("Got " + msg + "\n"); break;
+        enc1 = chr1 >> 2;
+        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+        enc4 = chr3 & 63;
+
+        if (isNaN(chr2)) {
+            enc3 = enc4 = 64;
+        } else if (isNaN(chr3)) {
+            enc4 = 64;
+        }
+        output += keyStr.charAt(enc1) + keyStr.charAt(enc2) +
+            keyStr.charAt(enc3) + keyStr.charAt(enc4);
+    }
+    return output;
+}
+
+var img = document.createElement('img');
+document.body.appendChild(img);
+
+window.onload = function() 
+{
+    s = new WebSocket("ws://poorhackers.com:9000/");
+    s.onopen = function(e) { console.log("opened");  }
+    s.onclose = function(e) { console.log("closed"); }    
+    s.binaryType = 'arraybuffer'
+
+s.onmessage = function(e) { 
+    var x=document.getElementById('data').rows
+    var y1 = x[0].cells
+    var y2 = x[1].cells
+    var y3 = x[2].cells
+
+    if (on_pic) {
+	on_pic = false;on_temp = true;
+	console.log("PIC: " + e.data);
+
+	var arrayBuffer = e.data;
+
+	img.src = 'data:image/jpeg;base64,' 
+	    + encode(new Uint8Array(arrayBuffer));
+    }
+    else if (on_temp) {
+	on_temp = false; on_hum = true;
+	y1[1].innerHTML = e.data + " F";
+	//console.log("TMP: " + e.data); 
+    }
+    else if (on_hum) {
+	on_hum = false; on_water = true;
+	y2[1].innerHTML= e.data + " %";
+	//console.log("HUMID: " + e.data); 	
+    }
+    else if (on_water) {
+	on_water = false;
+	on_pic = true;
+
+	if (e.data == "1") {
+	    y3[1].innerHTML="Absent";
+	}
+	else if (e.data == "0") {
+	    y3[1].innerHTML="Present";	    
+	}
+	else {
+	    y3[1].innerHTML="SENSOR ERROR, REFRESH.";	    
+	}
+
+	//console.log("water: " + e.data); 
     }
 }
+}
+
+function looper()
+{
+    s.send("x");
+}
+
+setInterval(looper, 5000);
